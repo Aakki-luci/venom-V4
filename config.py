0dd4c222a7b02419c613848ca59b2df8f5049987
Aakki-luci <mondalsubhankar503@gmail.com>
2024-07-31 20:50:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7389764306:AAF_jyFgiguaawR2gSuHSJj3UUfla1hG-Kw")
+BOT_TOKEN=("6712162128:AAEmDtIaMQZC25NzwxxxDtJea0cBCpzZQ0k")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("Aakash7201")
